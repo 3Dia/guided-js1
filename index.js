affe6c01b430e492b97fefc4a60bbfd515875e41
3Dia <tmgantt85@gmail.com>
2019-06-04 22:17:10 -0400

    understanding variables

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('Hey');
+
+//1st pass-keywords (var, let, const, function) indentifies and creates a reference in memmort
+// 2nd pass- assign by value - data to each of these keywords
+
+//String * 
+//Number * 
+//Boolean(Bool) * 
+//Null
+//Objects
+//Arrays
+//Functions
+//NaN
+
+'hey'
+
+//Variable
+   // var - avoid this
+   //let, const
+
+   var aVarariable //declared
+   aVariable = 12 // giving variable a value (initialize)
+   
